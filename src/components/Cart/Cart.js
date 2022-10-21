35da7887855890752173ab644303d788b40a539e
ahmedullah12 <gulamjoy321@gmail.com>
2022-10-21 17:25:11 +0600

    all added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import './Cart.css';
 
-const Cart = ({cart}) => {
+const Cart = ({cart, clearCart}) => {
+    
+    let total = 0;
+    let shipping = 0;
+    let quantity = 0;
+    for(const product of cart){
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+    const taxString = (total * 0.1).toFixed(2);
+    const tax = parseFloat(taxString);
+    const grandTotal = total + shipping + tax;
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summary</h3>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
+            <p>Total Price: ${total}</p>
+            <p>Total Shipping: ${shipping}</p>
+            <p>Tax: ${tax}</p>
+            <h5>Grand Total: ${grandTotal.toFixed(2)}</h5>
+            <button onClick={clearCart}>Clear Cart</button>
         </div>
     );
 };
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDcFhAMqTj-04bX28Bro0sBOfUZtPwzQws",
+  authDomain: "ema-john-simple-d08d7.firebaseapp.com",
+  projectId: "ema-john-simple-d08d7",
+  storageBucket: "ema-john-simple-d08d7.appspot.com",
+  messagingSenderId: "1029765805947",
+  appId: "1:1029765805947:web:cd2c06278aebab4fcf6fde"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
